dc4c682ababb653960e5f049ebc7e5d6306c0cb9
Tusshar-developer <tussharspinner@gmail.com>
2022-09-21 11:05:46 +1000

    Added warn command

--- commands/warn.js
+++ commands/warn.js
@@ -35,19 +35,54 @@ module.exports = {
       .setColor("#ff0000")
       .setDescription(`Reason: ${reason}`)
       .setTimestamp();
+
     await user.send({ embeds: [warnEmbed] });
+
+    const logEmbed = new Discord.EmbedBuilder()
+      .setTitle("User Warned")
+      .setColor("#ff0000")
+      .setDescription(
+        `**User:** <@${user.id}>\n**Moderator:** <@${interaction.user.id}>\n**Reason:** ${reason}`
+      )
+      .setTimestamp();
+
+    var checkLogChannel = await interaction.guild.channels
+      .fetch()
+      .then((channels) =>
+        channels.find((channel) => channel.name === "mod-logs")
+      );
+
+    if (!checkLogChannel) {
+      interaction.guild.channels.create({
+        name: "mod-logs",
+        type: 0,
+        permissionOverwrites: [
+          {
+            id: interaction.guild.roles.everyone,
+            deny: [Discord.PermissionFlagsBits.ViewChannel],
+          },
+        ],
+      });
+    }
+
+    var logChannel = await interaction.guild.channels
+      .fetch()
+      .then((channels) =>
+        channels.find((channel) => channel.name === "mod-logs")
+      );
+    await logChannel.send({ embeds: [logEmbed] });
+
+    const DMEmbed = new Discord.EmbedBuilder()
+      .setTitle("Warned " + user.tag)
+      .setColor("#ff0000")
+      .setDescription(`Reason: ${reason}`)
+      .setTimestamp();
+
+    await interaction.user.send({ embeds: [DMEmbed] });
+
     await interaction.reply({
       content: `Successfully warned <@${user.id}>`,
       ephemeral: true,
     });
-
-    await interaction.user.send(
-      user.tag +
-        ' has been warned for "' +
-        reason +
-        '" in "' +
-        interaction.guild.name +
-        '"'
-    );
   },
 };
