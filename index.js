36ada859b6d0599a8ffd80d46af5be85cef6a8c6
Tusshar-developer <tussharspinner@gmail.com>
2022-09-24 13:21:14 +1000

    Added firebase via firebase-sdk

--- index.js
+++ index.js
@@ -9,7 +9,29 @@ const { REST } = require("@discordjs/rest");
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 const fs = require("fs");
+const admin = require("firebase-admin");
+const { initializeApp } = require("firebase-admin/app");
+const { getFirestore } = require("firebase-admin/firestore");
+
 dotenv.config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
+
+initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+const db = getFirestore();
 
 const client = new Discord.Client({
   intents: [Discord.GatewayIntentBits.Guilds],
