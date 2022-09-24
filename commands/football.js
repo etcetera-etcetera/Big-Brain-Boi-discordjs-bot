ad5b8058595ca99c8242e0fe31bac802f96a8863
Tusshar-developer <tussharspinner@gmail.com>
2022-09-24 11:07:48 +1000

    reformatted commands/football.js

--- commands/football.js
+++ commands/football.js
@@ -19,6 +19,58 @@ const commandData = new SlashCommandBuilder()
       )
   );
 
+function standings(interaction) {
+  const league_id = interaction.options.getString("league_id");
+  var url = `https://api-football-standings.azharimm.dev/leagues/${league_id}/standings?sort=asc`;
+  request(url, async (err, response, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    var data = JSON.parse(body);
+    var standings = data.data.standings;
+
+    var embed = new Discord.EmbedBuilder()
+      .setTitle(data.data.name)
+      .setColor("#ff0000")
+      .setTimestamp();
+
+    try {
+      var table = new Table({
+        titles: ["Team", "GP", "W", "D", "L", "P", "GF", "GA", "GD"],
+        titleIndexes: [0, 30, 34, 38, 43, 50, 57, 62, 68],
+        columnIndexes: [0, 16, 19, 22, 25, 28, 32, 36, 40],
+        start: "`",
+        end: "`",
+        padEnd: 5,
+      });
+
+      for (var i = 0; i < standings.length; i++) {
+        table.addRow([
+          `${i + 1}. ${standings[i].team.abbreviation}`,
+          standings[i].stats[3].displayValue,
+          standings[i].stats[0].displayValue,
+          standings[i].stats[2].displayValue,
+          standings[i].stats[1].displayValue,
+          standings[i].stats[6].displayValue,
+          standings[i].stats[4].displayValue,
+          standings[i].stats[5].displayValue,
+          standings[i].stats[9].displayValue,
+        ]);
+      }
+
+      embed.addFields(table.field());
+
+      interaction.editReply({ embeds: [embed] });
+    } catch (err) {
+      console.log(err);
+      interaction.editReply(
+        "Something went wrong. Please try this command again later."
+      );
+    }
+  });
+}
+
 module.exports = {
   data: {
     help: "/football <subcommand>",
@@ -26,58 +78,11 @@ module.exports = {
   },
   async execute(interaction) {
     await interaction.deferReply();
-    if (interaction.options.getSubcommand() === "standings") {
-      const league_id = interaction.options.getString("league_id");
-      var url = `https://api-football-standings.azharimm.dev/leagues/${league_id}/standings?sort=asc`;
-      request(url, async (err, response, body) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        var data = JSON.parse(body);
-        var standings = data.data.standings;
-        console.log(standings);
 
-        console.log(standings.length);
-        var embed = new Discord.EmbedBuilder()
-          .setTitle(data.data.name)
-          .setColor("#ff0000")
-          .setTimestamp();
-
-        try {
-          var table = new Table({
-            titles: ["Team", "GP", "W", "D", "L", "P", "GF", "GA", "GD"],
-            // titleIndexes: [0, 39, 50, 58, 68, 77, 85, 92, 99],
-            // columnIndexes: [0, 16, 22, 27, 33, 38, 45, 50, 54],
-            titleIndexes: [0, 30, 34, 38, 43, 50, 57, 62, 68],
-            columnIndexes: [0, 16, 19, 22, 25, 28, 32, 36, 40],
-            start: "`",
-            end: "`",
-            padEnd: 5,
-          });
-
-          for (var i = 0; i < standings.length; i++) {
-            table.addRow([
-              `${i + 1}. ${standings[i].team.abbreviation}`,
-              standings[i].stats[3].displayValue,
-              standings[i].stats[0].displayValue,
-              standings[i].stats[2].displayValue,
-              standings[i].stats[1].displayValue,
-              standings[i].stats[6].displayValue,
-              standings[i].stats[4].displayValue,
-              standings[i].stats[5].displayValue,
-              standings[i].stats[9].displayValue,
-            ]);
-          }
-
-          embed.addFields(table.field());
-
-          interaction.editReply({ embeds: [embed] });
-        } catch (err) {
-          console.log(err);
-          interaction.editReply("bruh");
-        }
-      });
+    switch (interaction.options.getSubcommand()) {
+      case "standings":
+        standings(interaction);
+        break;
     }
   },
 };
