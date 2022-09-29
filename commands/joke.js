8ba85a644ab9df317313fbd8e5329901f7055dc7
M Singh <teghjyot@icloud.com>
2022-09-29 19:28:54 +1000

    Second instance - all 3 JSON files working

--- commands/joke.js
+++ commands/joke.js
@@ -2,32 +2,53 @@ const Discord = require("discord.js");
 const { SlashCommandBuilder } = Discord;
 const request = require("request");
 const fs = require("fs");
-const json = fs.readFileSync("./reddit_jokes.json");
-const jokeSON = JSON.parse(json);
 const { EmbedBuilder } = require("discord.js");
+const filesJSON = ["./reddit_jokes.json", "./stupidstuff.json", "./wocka.json"];
+// for loop all the JSON files and read them using file sync
+var masterSON = [];
+let upvotes = " upvotes ";
+
+for (i = 0; i < filesJSON.length; i++) {
+  var readSON = fs.readFileSync(filesJSON[i], "utf8");
+  var jokeSON = JSON.parse(readSON);
+  // push to masterSON
+  masterSON = masterSON.concat(jokeSON);
+}
 
 var commandData = new SlashCommandBuilder()
   .setName("joke")
   .setDescription("Funny?");
 
 async function joke(interaction) {
-  const joke = jokeSON[Math.floor(Math.random() * jokeSON.length)];
-  let title = joke.title;
-  let body = joke.body;
-  let score = joke.score;
-  let id = joke.id;
-  let upvotes = " upvotes ";
-  if (score == 1) {
-    let upvotes = " upvote ";
+  const joke = masterSON[Math.floor(Math.random() * masterSON.length)];
+
+  if (joke.rating) {
+    color = "#ff0000";
+    title = joke.category;
+    body = joke.body;
+    footer = { text: joke.rating + "/5  | " + joke.id };
+  } else if (joke.score > -1) {
+    color = "#ff0000";
+    title = joke.title;
+    body = joke.body;
+    if (joke.score == 1) {
+      upvotes = " upvote ";
+    }
+    console.log(joke.score);
+    footer = { text: joke.score + upvotes + " | " + joke.id };
+  } else {
+    color = "#ff0000";
+    title = joke.title;
+    body = joke.body;
+    console.log(typeof joke.category);
+    footer = { text: joke.category + " Joke no. " + joke.id };
   }
 
   const jokesEmbed = new EmbedBuilder()
-    .setColor(0x0099ff)
+    .setColor(color)
     .setTitle(title)
     .setDescription(body)
-    .setFooter({
-      text: score + upvotes + " | " + id,
-    })
+    .setFooter(footer)
     .setTimestamp();
 
   interaction.reply({ embeds: [jokesEmbed] });
